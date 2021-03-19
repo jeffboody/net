c421941cf4894a8a4326a2edd97f7ad7ea8099d6
Jeff Boody <jeffboody@gmail.com>
2021-03-19 16:38:28 -0600

    fix common name (CN) for SSL certs

--- sslkeys/make-sslkeys.sh
+++ sslkeys/make-sslkeys.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # ca_cert.pem
+# The CN of the root CA is your name.
 mkdir -p ca/private
 chmod 700 ca/private
 openssl req \
@@ -10,25 +11,27 @@ openssl req \
     -newkey rsa:4096 \
     -keyout ca/private/ca_key.pem \
     -out ca/ca_cert.pem \
-    -subj "/C=US/ST=Colorado/L=Boulder/CN=jeffboody.com/emailAddress=jeffboody@gmail.com"
+    -subj "/C=US/ST=Colorado/L=Boulder/CN=Jeff Boody/emailAddress=jeffboody@gmail.com"
 
 # server_key.pem
+# The server CN must be a fully qualified domain name.
 mkdir -p server/private
 chmod 700 server/private
 openssl genrsa -out server/private/server_key.pem 4096
 openssl req -new \
     -key server/private/server_key.pem \
     -out server/server.csr \
-    -subj "/C=US/ST=Colorado/L=Boulder/CN=server.jeffboody.com/emailAddress=jeffboody@gmail.com"
+    -subj "/C=US/ST=Colorado/L=Boulder/CN=localhost/emailAddress=jeffboody@gmail.com"
 
 # client_key.pem
+# The client CN may be any unique identifier.
 mkdir -p client/private
 chmod 700 client/private
 openssl genrsa -out client/private/client_key.pem 4096
 openssl req -new \
     -key client/private/client_key.pem \
     -out client/client.csr \
-    -subj "/C=US/ST=Colorado/L=Boulder/CN=client.jeffboody.com/emailAddress=jeffboody@gmail.com"
+    -subj "/C=US/ST=Colorado/L=Boulder/CN=Net Client App/emailAddress=jeffboody@gmail.com"
 
 # server_cert.pem
 openssl x509 -req -days 3650 -in server/server.csr \
